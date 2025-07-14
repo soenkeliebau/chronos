115aacbba5a82cae300a1033011c1e6f49889523
Sönke Liebau <soenke.liebau@stackable.tech>
2025-07-14 21:10:56 +0200

    Added serious-business as dependency.

--- src/ChronoConfig.rs
+++ src/ChronoConfig.rs
@@ -1,9 +1,13 @@
-use crate::ChronoConfig::Error::{GetEnvVar, GetPasswordFromEntry};
+use crate::ChronoConfig::Error::{GetEnvVar, GetPasswordFromEntry, NoPassword};
 use crate::Favorite;
+use inquire::{Password, PasswordDisplayMode};
 use keyring::Entry;
+use keyring::Error::NoEntry;
+use secrecy::SecretString;
 use serde::Deserialize;
 use snafu::{ResultExt, Snafu};
 
+static PASSWORD_ENV_VAR: &str = "CHRONOS_PASSWORD";
 static CONFIG_FILE_LOCATION_OVERRIDE: &str = "CHRONOS_CONFIG_FILE";
 static CONFIG_FILE_LOCATION_DEFAULT: &str = "~/.chronos/config.json";
 
@@ -24,20 +28,18 @@ pub enum Error {
         user: String,
     },
     #[snafu(display("error getting password from keyring response: {source}"))]
-    GetPasswordFromEntry {
-        source: keyring::Error,
-    },
+    GetPasswordFromEntry { source: keyring::Error },
+    #[snafu(display("error storing password into keyring : {source}"))]
+    SetPasswordFromEntry { source: keyring::Error },
+    #[snafu(display("No password provided, please either set one via env var, the config, or use `login` to store one in the systems keyring."))]
+    NoPassword { },
+    
+
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    pub password: String,
-    pub user_name: String,
-    pub favorites: Option<Vec<Favorite>>,
-}
-
-#[derive(Deserialize, Debug)]
-struct InternalConfig {
+    pub password: Option<SecretString>,
     pub user_name: String,
     pub favorites: Option<Vec<Favorite>>,
 }
@@ -47,22 +49,75 @@ impl Config {
         let config_location = std::env::var(CONFIG_FILE_LOCATION_OVERRIDE)
             .unwrap_or(CONFIG_FILE_LOCATION_DEFAULT.to_string());
 
-        let config_from_file: InternalConfig = serde_json::from_str(
+        let mut config_from_file: Config = serde_json::from_str(
             &std::fs::read_to_string(&config_location).context(ReadConfigFileSnafu {
                 path: &config_location,
             })?,
         )
         .context(ParseConfigFileSnafu)?;
 
-        let test = Entry::new("tech.stackable.chronos", &config_from_file.user_name).context(
-            GetPasswordSnafu {
-                user: &config_from_file.user_name,
-            },
-        )?;
-        Ok(Self {
-            password: test.get_password().context(GetPasswordFromEntrySnafu {})?,
-            user_name: config_from_file.user_name,
-            favorites: config_from_file.favorites,
-        })
+        // Priority for password config is:
+        //   1. env var
+        //   2. config file
+        //   3. keyring
+        // So we check if the env var is set, if yes we overwrite whatever was loaded from the file,
+        // if no env var is present we check if the password from the file is Some(..) - if yes we
+        // use that, if not we fall back to keyring
+        if let Some(password_from_env) = std::env::var(PASSWORD_ENV_VAR).ok() {
+            config_from_file.password = Some(SecretString::from(password_from_env));
+        } else {
+            // No password from env var, so check if we got one from the file
+            if config_from_file.password.is_none() {
+                // no password in the file, read from keyring
+                let password =
+                    match Entry::new("tech.stackable.chronos", &config_from_file.user_name) {
+                        Ok(password_entry) => {
+                            if let Ok(password) =
+                                password_entry.get_password().context(GetPasswordSnafu {
+                                    user: &config_from_file.user_name,
+                                })
+                            {
+                                config_from_file.password = Some(SecretString::from(password));
+                            } else {
+                                return Err(NoPassword {});
+                                println!("No stored password found.");
+                                let new_password = Password::new(&format!(
+                                    "Enter password for user {}: ",
+                                    &config_from_file.user_name
+                                ))
+                                .with_display_toggle_enabled()
+                                .without_confirmation()
+                                .with_display_mode(PasswordDisplayMode::Hidden)
+                                .with_formatter(&|_| String::from("Input received"))
+                                .prompt()
+                                .context(ObtainPasswordFromUserSnafu {})?;
+                                password_entry
+                                    .set_password(&new_password)
+                                    .context(SetPasswordFromEntrySnafu {})?;
+                            };
+                        }
+
+                        Err(e) => match e {
+                            NoEntry => {
+                                println!("No password entry found!")
+                            }
+                            _ => {
+                                println!("Error obtaining password!")
+                            }
+                        },
+                    };
+            }
+        }
+        Ok(config_from_file)
+    }
+    
+    pub fn save_password(&self, password: &str) -> Result<(), Error> {
+        match Entry::new("tech.stackable.chronos", &self.user_name) {
+            Ok(entry) => {
+                entry.set_password(password).context(SetPasswordFromEntrySnafu)?;
+            }
+            Err(_) => {}
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod ChronoConfig;
 
+use ChronoConfig::Config;
+use chrono::NaiveDate;
 use clap::Parser;
+use inquire::{Password, PasswordDisplayMode};
+use secrecy::ExposeSecret;
 use serde::Deserialize;
 use snafu::{ResultExt, Snafu};
-use ChronoConfig::Config;
+use std::time::Duration;
 
 #[derive(Snafu, Debug)]
 pub enum Error {
     #[snafu(display("Error reading configuration: {source}"))]
-    Config {
-        source: crate::ChronoConfig::Error,
-    },
+    Config { source: crate::ChronoConfig::Error },
+    #[snafu(display("failed to read password input from user : {source}"))]
+    ObtainPasswordFromUser { source: inquire::InquireError },
 }
 
 #[derive(Parser, Debug)]
@@ -18,38 +22,68 @@ pub enum Error {
 struct Args {
     #[clap(subcommand)]
     command: Command,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
 }
 
 #[derive(Parser, Debug)]
 enum Command {
     Login,
     Run,
-    Book,
+    Book {
+        #[arg(short, long)]
+        template: Option<String>,
+        #[arg(short, long)]
+        project: Option<String>,
+        #[arg(short, long)]
+        task: Option<String>,
+        #[arg(short, long)]
+        duration: Option<Duration>,
+        #[arg(short, long)]
+        reference: Option<String>,
+        #[arg(short, long)]
+        comment: Option<String>,
+        #[arg(short, long)]
+        date: Option<NaiveDate>,
+    },
+    Sync,
 }
 
 #[derive(Parser, Debug, Deserialize)]
-pub struct Favorite{
+pub struct Favorite {
     project: usize,
     task: usize,
     duration: Option<usize>,
     comment: Option<String>,
 }
 
-fn main() -> Result<(), Error>{
+fn main() -> Result<(), Error> {
     let args = Args::parse();
 
     let config = Config::new().context(ConfigSnafu {})?;
-    
+
     match args.command {
         Command::Login => {
-            println!("Logging in for user {}", config.user_name);
+            let new_password =
+                Password::new(&format!("Enter password for user {}: ", &config.user_name))
+                    .with_display_toggle_enabled()
+                    .without_confirmation()
+                    .with_display_mode(PasswordDisplayMode::Hidden)
+                    .with_formatter(&|_| String::from("Input received"))
+                    .prompt()
+                    .context(ObtainPasswordFromUserSnafu {})?;
+            config.save_password(&new_password)?;
+            println!("{}", config.password.unwrap().expose_secret());
         }
         Command::Run => {}
-        Command::Book => {}
+        Command::Book {
+            template,
+            project,
+            task,
+            duration,
+            reference,
+            comment,
+            date,
+        } => {}
+        Command::Sync => {}
     }
     Ok(())
 }
